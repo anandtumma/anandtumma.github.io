66c37038f9f82b78ae7ca8e651dbbcccbcc327ad
anandtumma <168094507+anandtumma@users.noreply.github.com>
2025-03-26 16:47:31 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -243,19 +243,7 @@ Description: Gerold - Personal Portfolio HTML5 Template
 		/*------------------------------------------------------
   	/  Preloader
   	/------------------------------------------------------*/
-		<script>
-		    document.addEventListener("DOMContentLoaded", function() {
-		        let progressBar = document.getElementById("progress-bar");
 		
-		        setTimeout(() => {
-		            progressBar.style.width = "100%";
-		        }, 500); // Start after a short delay
-		
-		        setTimeout(() => {
-		            document.querySelector(".preloader").style.display = "none";
-		        }, 2500); // Hide preloader after animation
-		    });
-		</script>
 		
 		const svg = document.getElementById("preloaderSvg");
 		const svgText = document.querySelector(
