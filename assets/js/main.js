266cf4d719fc9557ec2cf528856e2c2934397696
anandtumma <168094507+anandtumma@users.noreply.github.com>
2025-03-26 16:38:35 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -243,42 +243,56 @@ Description: Gerold - Personal Portfolio HTML5 Template
 		/*------------------------------------------------------
   	/  Preloader
   	/------------------------------------------------------*/
-		const svg = document.getElementById("preloaderSvg");
-		const svgText = document.querySelector(
-			".hero-section .intro_text svg text"
-		);
-		const tl = gsap.timeline({
-			onComplete: startStrokeAnimation,
-		});
-		const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
-		const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
+		<script>
+		    document.addEventListener("DOMContentLoaded", function() {
+		        let progressBar = document.getElementById("progress-bar");
+		
+		        setTimeout(() => {
+		            progressBar.style.width = "100%";
+		        }, 500); // Start after a short delay
+		
+		        setTimeout(() => {
+		            document.querySelector(".preloader").style.display = "none";
+		        }, 2500); // Hide preloader after animation
+		    });
+		</script>
+		
+		// const svg = document.getElementById("preloaderSvg");
+		// const svgText = document.querySelector(
+		// 	".hero-section .intro_text svg text"
+		// );
+		// const tl = gsap.timeline({
+		// 	onComplete: startStrokeAnimation,
+		// });
+		// const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
+		// const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
 
-		tl.to(".preloader-heading .load-text , .preloader-heading .cont", {
-			delay: 1.5,
-			y: -100,
-			opacity: 0,
-		});
-		tl.to(svg, {
-			duration: 0.5,
-			attr: { d: curve },
-			ease: "power2.easeIn",
-		}).to(svg, {
-			duration: 0.5,
-			attr: { d: flat },
-			ease: "power2.easeOut",
-		});
-		tl.to(".preloader", {
-			y: -1500,
-		});
-		tl.to(".preloader", {
-			zIndex: -1,
-			display: "none",
-		});
+		// tl.to(".preloader-heading .load-text , .preloader-heading .cont", {
+		// 	delay: 1.5,
+		// 	y: -100,
+		// 	opacity: 0,
+		// });
+		// tl.to(svg, {
+		// 	duration: 0.5,
+		// 	attr: { d: curve },
+		// 	ease: "power2.easeIn",
+		// }).to(svg, {
+		// 	duration: 0.5,
+		// 	attr: { d: flat },
+		// 	ease: "power2.easeOut",
+		// });
+		// tl.to(".preloader", {
+		// 	y: -1500,
+		// });
+		// tl.to(".preloader", {
+		// 	zIndex: -1,
+		// 	display: "none",
+		// });
 
-		function startStrokeAnimation() {
-			// Add a class or directly apply styles to trigger the stroke animation
-			svgText.classList.add("animate-stroke");
-		}
+		// function startStrokeAnimation() {
+		// 	// Add a class or directly apply styles to trigger the stroke animation
+		// 	svgText.classList.add("animate-stroke");
+		// }
 
 		/*------------------------------------------------------
   	/  Services Hover BG
